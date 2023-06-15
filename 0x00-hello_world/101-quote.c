7de76f3587abb8b9a329f707718a236954580e1b
Wiztech-01 <lynnnyaoke@gmail.com>
2023-06-15 15:43:17 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (0);
 }	
