20ef96d95f80de26e1d2c41e24c955dfda30cde5
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-06 01:17:50 +0000

    a function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int sqr_root(int base, int root)
 {
 	return (
 			(root * root == base) ? root :
-			(root == base / 2) ? -1 i:
+			(root == base / 2) ? -1 i :
 			sqr_root(base, root + 1)
 		);
 }
