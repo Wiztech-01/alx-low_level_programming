f9d27c133944ad4d9779f69108f6024e0d916546
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-07 17:11:58 +0000

    a program that adds poaitive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@
   */
 int main(int argc,  char *argv[])
 {
-	in, j, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
@@ -28,6 +28,7 @@ int main(int argc,  char *argv[])
 			{
 				printf("Error\n");
 				return (1);
+			}
 		}
 
 		sum += atoi(argv[i]);
