b47e882729f4fb5c780e332db3439d09e2448754
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-11 10:03:27 -0700

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,19 +1,55 @@
-#include <stdlib.h>
 #include "main.h"
-#include "2-strlen.c"
+#include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- * of an array of chars
- * @str: string to be copied
+ *_strlen - Returns the length of a string
+ *@s: The string being used
+ *Return: Returns the length of the string
+ */
+
+int _strlen(char *s)
+{
+	unsigned int len = 0;
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		len += sizeof(char);
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: destination
+ * @src: source
  *
- * Return: a pointer to the newly allocated space in memory
+ * Return: dest/copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+
+/**
+ *_strdup - Copies a String given as a parameter
+ *@str: The string to be copied
+ *Return: A pointer to the new string on Success
  */
 
 char *_strdup(char *str)
 {
 	char *dup;
-	int i = 0;
 
 	if (str == NULL)
 		return (NULL);
@@ -23,8 +59,7 @@ char *_strdup(char *str)
 	if (dup == NULL)
 		return (NULL);
 
-	for (; str[i] != '\0'; i++)
-		dup[i] = str[i];
+	_strcpy(dup, str);
 
 	return (dup);
 }
