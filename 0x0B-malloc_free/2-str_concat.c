a293529cf0da091f59e9b936bdedd5b5782f8dc4
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-11 10:06:30 -0700

    a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,99 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _strlen - length of a string
- * @s: char param
- * Return: str length
+ *_strlen - Returns the length of a string
+ *@s: The string being used
+ *Return: Returns the length of the string
  */
+
 int _strlen(char *s)
 {
-	int l = 0;
+	unsigned int len = 0;
+	int i;
 
-	while (*s != '\0')
-	{
-		s++;
-		l++;
-	}
-	return (l);
+	for (i = 0; *(s + i) != '\0'; i++)
+		len += sizeof(char);
+
+	return (len);
 }
 
 /**
- * str_concat - concat two str
- * @s1: str1
- * @s2: str2
- * Return: char
+ * _strcpy - copies a string from src to dest
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest/copied string
  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ *_strcat - adds to strings
+ *@dest: destination
+ *@src: source
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ *str_concat - Concatenates two strings
+ *@s1: The first string
+ *@s2: The second string
+ *Return: A pointer that contains s1, s2 followed with a null
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int l1, l2;
-	char *conc, *tmp;
+	char *conc;
+	size_t len1;
+	size_t len2;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	else
-		l1 = _strlen(s1);
-
-	if (!s2)
+	if (s2 == NULL)
 		s2 = "";
-	else
-		l2 = _strlen(s2);
 
-	conc = malloc(l1 + l2 + 1);
-	if (!conc)
-		return (0);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
-	tmp = conc;
-	while (*s1)
-		*tmp++ = *s1++;
+	conc = malloc((len1 + len2 + 1) * sizeof(char));
 
-	while ((*tmp++ = *s2++))
-		;
+	if (conc == NULL)
+		return (NULL);
+
+	_strcpy(conc, s1);
+	_strcat(conc, s2);
+
 	return (conc);
 }
