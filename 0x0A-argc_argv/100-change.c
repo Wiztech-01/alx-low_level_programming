3ddc4f2008648b231f522b12bff0fcefe056fde9
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-13 04:31:21 -0700

    a program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,55 +2,41 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make
- * for an amount of money
+ * main - prints the minimum number of coins to make change for an amount
+ *	    if c != 1, print Error
+ *	    f the number passed as the argument is negative, print 0
+ *	    You can use coins of values 25, 10, 5, 2, and 1 cent
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: returns 1 if here is an error; else returns 0
+ *
+ * Return: 1 if argc != 1, 0 otherwise
  */
 
 int main(int argc, char *argv[])
 {
-int cents, coins = 0;
+	int amnt, coins[5] = {25, 10, 5, 2, 1};
+	int i = 0, count = 0, ln = sizeof(coins) / sizeof(coins[0]);
 
-if (argc != 2)
-{
-	printf("Error\n");
-	return (1);
-}
-cents = atoi(argv[1]);
-while (cents > 0)
-{
-	coins++;
-	if ((cents - 25) >= 0)
+	if (argc == 1)
 	{
-		cents -= 25;
-		continue;
+		printf("Error\n");
+		return (1);
 	}
-	if ((cents - 10) >= 0)
-	{
-		cents -= 10;
-		continue;
-	}
-	if ((cents -= 5) >= 0)
-	{
-		cents -= 5;
-		continue;
-	}
-	if ((cents - 5) >= 0)
-	{
-		cents -= 5;
-		continue;
-	}
-	if ((cents - 2) >= 0)
-	{
-		cents -= 2;
-		continue;
-	}
-        cents--;
-}
-printf("%d\n", coins);
-return (0);
-}
 
+	amnt = atoi(argv[1]);
 
+	if (argc == 2 && amnt >= 0)
+	{
+		for (i = 0; i < ln; i++)
+		{
+			count += amnt / coins[i];
+			amnt %= coins[i];
+		}
+		printf("%d\n", count);
+	}
+
+	else
+		printf("0\n");
+
+	return (0);
+}
