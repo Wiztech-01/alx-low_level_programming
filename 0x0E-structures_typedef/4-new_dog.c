e87684d38a1fa2205806f1636aeb335e2cb80c73
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-18 05:57:55 -0700

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,28 +1,81 @@
 #include "dog.h"
 #include <stdlib.h>
-#include "1-strdup.c"
 
 
 /**
- *new_dog - Pointer to the struct dog_t
- *@name: Pointer to the name of the dog
- *@age: The age of the dog
- *@owner: Pointer to the name of the owner of the dog
- *Return: A Pointer to the struct dog_t
- */
+*_strlen - returns length of
+*a string
+*@str: string to be counted
+*Return: returns length of string
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (str)
+len++;
 
+return (len);
+}
+
+
+/**
+*_strcopy - copy string pointed by src
+*into dest variable
+*@dest:buffer storing string copy
+*@src: buffer storing string to copy
+*Return:returns copied string
+*/
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+
+for (; src[index] ; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
+
+
+
+
+/**
+*new_dog - creates a new dog
+*@name: name of new dog
+*@age: age of new dog
+*@owner: owner of new dog
+*Return: returns NULL in case
+*of failure
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+dog_t *doggo;
 
-	dog = malloc(sizeof(dog_t));
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
 
-	if (dog == NULL)
-		return (NULL);
+doggo = malloc(sizeof(dog_t));
+if (doggo == NULL)
+return (NULL);
 
-	dog->name = _strdup(name);
-	dog->age = age;
-	dog->owner = _strdup(owner);
+doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (doggo->name == NULL)
+{
+free(doggo);
+return (NULL);
+}
 
-	return (dog);
+doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (doggo->owner == NULL)
+{
+free(doggo->name);
+free(doggo);
+return (NULL);
+}
+
+doggo->name = _strcopy(doggo->name, name);
+doggo->age = age;
+doggo->owner = _strcopy(doggo->owner, owner);
+
+return (doggo);
 }
