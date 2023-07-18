35c74cdf47f2568b560cea452762f927bb8f8c32
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-18 05:51:38 -0700

    a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,47 +1,28 @@
-#include <stdlib.h>
 #include "dog.h"
-#include "2-strlen.c"
-#include "9-strcpy.c"
+#include <stdlib.h>
+#include "1-strdup.c"
+
 
 /**
- * new_dog - creates a new dog struct
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: A new dog struct
+ *new_dog - Pointer to the struct dog_t
+ *@name: Pointer to the name of the dog
+ *@age: The age of the dog
+ *@owner: Pointer to the name of the owner of the dog
+ *Return: A Pointer to the struct dog_t
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *bosco;
+	dog_t *dog;
 
-	if (name == NULL || age < 0 || owner == NULL)
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
 		return (NULL);
 
-	bosco = malloc(sizeof(dog_t));
+	dog->name = _strdup(name);
+	dog->age = age;
+	dog->owner = _strdup(owner);
 
-	if (bosco == NULL)
-		return (NULL);
-
-	bosco->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (bosco->name == NULL)
-	{
-		free(bosco);
-		return (NULL);
-	}
-
-	bosco->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (bosco->owner == NULL)
-	{
-		free(bosco->name);
-		free(bosco);
-		return (NULL);
-	}
-
-	bosco->name = _strcpy(bosco->name, name);
-	bosco->age = age;
-	bosco->owner = _strcpy(bosco->owner, owner);
-
-	return (bosco);
+	return (dog);
 }
