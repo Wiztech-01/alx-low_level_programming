2c033bd7f11575ea9093e85b22e06edbcf035cc6
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-18 05:45:53 -0700

    a function that frees dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,81 +1,28 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
-/**
- * _strlen - returns the length of a string
- *@s: pointer of character
- *Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int len;
+#include "1-strdup.c"
 
-	len = 0;
-	while (*(s + len) != '\0')
-		len++;
-	return (len);
-}
 
 /**
- * *_strcpy - copies the string pointed
- *@dest: copie of the string
- *@src: origin of the string
- *Return: dest destine copie
+ *new_dog - Pointer to the struct dog_t
+ *@name: Pointer to the name of the dog
+ *@age: The age of the dog
+ *@owner: Pointer to the name of the owner of the dog
+ *Return: A Pointer to the struct dog_t
  */
-char *_strcpy(char *dest, char *src)
-{
-	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
- * new_dog - function to create a new dog
- *@name: name of the dog
- *@age: age of the dog
- *@owner: name of owner of the dog
- *Return: new dog created
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *cname, *cowner;
+	dog_t *dog;
 
-	new_dog = malloc(sizeof(struct dog));
+	dog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (dog == NULL)
 		return (NULL);
 
-	cname = malloc(sizeof(char *) * (_strlen(name) + 1));
+	dog->name = _strdup(name);
+	dog->age = age;
+	dog->owner = _strdup(owner);
 
-	if (cname == NULL)
-	{
-		free(cname);
-		free(new_dog);
-		return (NULL);
-	}
-
-	cname = _strcpy(cname, name);
-
-	cowner = malloc(sizeof(char *) * (_strlen(owner) + 1));
-	if (cowner == NULL)
-	{
-		free(cowner);
-		free(cname);
-		free(new_dog);
-		return (NULL);
-	}
-
-	cowner = _strcpy(cowner, owner);
-
-	new_dog->name = cname;
-	new_dog->age = age;
-	new_dog->owner = cowner;
-
-	return (new_dog);
+	return (dog);
 }
