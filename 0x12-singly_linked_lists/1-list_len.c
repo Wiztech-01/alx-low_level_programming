088a90e1c8187d82889271e74685db669d7978fe
Wiztech-01 <lynnnyaoke@gmail.com>
2023-07-27 18:11:30 +0000

    a function that returns the number of elements in a linked list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,45 +1,20 @@
+i#include <stdlib.h>
 #include "lists.h"
-#include <string.h>
 
 /**
- *_strlen - Returns the length of a string
- *@s: The string being used
- *Return: Returns the length of the string
- */
-
-int _strlen(const char *s)
-{
-	unsigned int len = 0;
-	int i;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-		len += sizeof(char);
-
-	return (len);
-}
-
-/**
- *add_node - adds a new node at the beginning of a list_t list
- *@head: Head of the list_t
- *@str: Pointer to the value to insert in the new element
+ * list_len - returns the number of members in a linked list
+ * @h: pointer to the list
  *
- *Return: The address of the new element
+ * Return: number of h
  */
-
-list_t *add_node(list_t **head, const char *str)
+size_t list_len(const list_t *h)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	size_t n = 0;
 
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
-
-	new_node->len = _strlen(str);
-
-	new_node->next = *head;
-
-	*head = new_node;
-
-	return (new_node);
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
