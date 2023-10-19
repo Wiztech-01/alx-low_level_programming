023d29230613aa45c63f7c8509e8934f3dadf9a4
Wiztech-01 <lynnnyaoke@gmail.com>
2023-10-19 07:45:49 +0000

    updated

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a+b);
+}
+
+int sub(int a, int b)
+{
+	return (a-b);
+}
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
+
+int div(int a, int b)
+{
+	return (a/b);
+}
+
+int mod(int a, int b)
+{
+	return (a%b);
+}
+
